9df67cacfac39d0a674c713b0e72d28cb07409f2
Chaoui <mchaoui018@gmaill.com>
2026-05-16 12:34:30 +0100

    Structuration du Panier

--- FRONT_END/src/Component/Panier.jsx
+++ FRONT_END/src/Component/Panier.jsx
@@ -2,7 +2,7 @@ function Panier () {
 
     return(
         <div>
-                        <h1 className="VPanier" >VOTRE PANIER :</h1>
+                        <h1  className="VPanier" >VOTRE PANIER :</h1>
 
         <div className="articleETaside" >
             <article className="articl" >
@@ -12,13 +12,31 @@ function Panier () {
                     <th>PRODUIT</th>
                     <th>PRIX</th>
                     <th>QUANTITÉE</th> 
+                    <th>SOUS TOTAL</th>
+                    <th> 🗑</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-
-
-                        
+                    <td> PRODUITE 1  </td>
+                    <td> PRIX PRODUIT 1  </td>
+                    <td> QTE PRODUITE 1  </td>  
+                    <td>MONTANT UNITAIRE</td>    
+                    <td>X</td>                 
+                    </tr>
+                     <tr>
+                    <td> PRODUITE 1  </td>
+                    <td> PRIX PRODUIT 1  </td>
+                    <td> QTE PRODUITE 1  </td>      
+                    <td>MONTANT UNITAIRE</td>    
+                    <td>X</td>             
+                    </tr>
+                     <tr>
+                    <td> PRODUITE 1  </td>
+                    <td> PRIX PRODUIT 1  </td>
+                    <td> QTE PRODUITE 1  </td>                       
+                    <td>MONTANT UNITAIRE</td>    
+                    <td>X</td> 
                     </tr>
                 </tbody>
                 <tfoot>
@@ -31,6 +49,9 @@ function Panier () {
             </article>
             <aside className="side" >
                 <h1>TOTAL TTC : </h1>
+                <span>Le Montant</span><br />
+                <input type="button" value="PASSER LA COMMANDE"  ></input>
+                <button>ENREGISTRER LA COMMANDE</button>
             </aside>
         </div>
         </div>
