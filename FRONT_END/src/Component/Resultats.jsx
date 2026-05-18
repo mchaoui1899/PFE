3c8ec59a91981f102add026d80c5a0cc5532103f
Chaoui <mchaoui018@gmaill.com>
2026-05-18 16:26:02 +0100

    Toujours Recherche resultats

--- FRONT_END/src/Component/Resultats.jsx
+++ FRONT_END/src/Component/Resultats.jsx
@@ -1,18 +1,24 @@
 import { useState } from "react";
 import {BrowserRouter ,Route , Link ,Routes } from 'react-router-dom' ;
-
-
 function Resultats (){
+
+  const [resultats , setResultats ]=useState("Produits");
+  
+
 return(
-    
-    <div className="resultatsRecherche" > 
-        <h1>Les résultats pour {} dans le rayon {}</h1>
+        <div className="resultatsRecherche" > 
+        <h1>Les résultats pour X{} dans le rayon Y{}</h1>
         <div>
-            <span>{}Produits</span>  <span>  {}Catégorie</span> <span>  {}Marque</span>
+            <span className="resultatsProduits" onClick={()=>{setResultats("Produits")}} >{}Produits</span> 
+             <span className="resultatsCatégories" onClick={ ()=>{setResultats("Catégories")} }>  {}Catégories</span>
+              <span className="resultatsMarque" onClick={()=>{ setResultats("Marque")   }}>  {}Marques</span>
             <div>
                 <fieldset>
                     <div>
-                        
+                        {  resultats==="Produits" && <div className="ResultatsProduits" > DIV DES PRODUITS </div>  }
+                        {  resultats==="Catégories" && <div className="ResultatsCategorie" > DIV DES Catégories </div>  }
+                        {  resultats==="Marque" && <div className="ResultatsMarque" > DIV DES Marques </div>  }
+
                     </div>
                 </fieldset>
             </div>
@@ -20,8 +26,6 @@ return(
         </div>
 
     </div>
-
-
 
 );
 
